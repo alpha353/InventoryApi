5e8346ba85df046fcf7f8a0d515090a4bcee6c60
Suraj ... <sqadalpha@gmail.com>
2026-02-09 22:58:50 +0530

    app started successfully(api is active)

--- obj/Debug/net10.0/InventoryApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/InventoryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b0c6b987e06064c426c51209344f942b39d24b")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
